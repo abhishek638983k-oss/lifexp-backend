6fed37f80243c23b5929e91c5d3fd015f89078d8
abhishek638983k-oss <abhishek638983k@gmail.com>
2026-04-11 12:57:26 +0530

    Fix formatting in User model for friendRequests

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     lastCompleted: Date,
 
    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-   friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+   friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     categoryXP: {
         coding: { type: Number, default: 0 },
         fitness: { type: Number, default: 0 },
