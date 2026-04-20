c1d67c1365f9c6e07ae41b15aa785e161f366f1f
abhishek638983k-oss <abhishek638983k@gmail.com>
2026-04-21 00:44:41 +0530

    Add authentication middleware to leaderboard route

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const { getLeaderboard } = require("../controllers/leaderboardController");
 
-router.get("/", getLeaderboard);
+router.get("/", authMiddleware, getLeaderboard);
 
 module.exports = router;
