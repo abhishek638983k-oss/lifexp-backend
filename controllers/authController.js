d8f99de7901d68ac36955bb4a4d924c819752078
abhishek638983k-oss <abhishek638983k@gmail.com>
2026-04-21 00:23:39 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 // SIGNUP
 const signup = async (req, res) => {
     try {
@@ -7,13 +8,19 @@ const signup = async (req, res) => {
 
         const existing = await User.findOne({ username });
         if (existing) {
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ message: "User exists" });
         }
 
-        const user = new User({ username, password });
+        const hashed = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            username,
+            password: hashed
+        });
+
         await user.save();
 
-        res.json({ message: "Signup success", user });
+        res.json({ message: "Signup success" });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -24,10 +31,15 @@ const login = async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        const user = await User.findOne({ username, password });
-
+        const user = await User.findOne({ username });
         if (!user) {
-            return res.status(400).json({ message: "Invalid credentials" });
+            return res.status(400).json({ message: "Invalid" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid" });
         }
 
         res.json({ message: "Login success", user });
@@ -35,5 +47,11 @@ const login = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+const token = jwt.sign(
+    { id: user._id },
+    "secretkey",
+    { expiresIn: "7d" }
+);
 
+res.json({ token, user });
 module.exports = { signup, login };
